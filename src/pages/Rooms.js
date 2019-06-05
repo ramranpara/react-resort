d49997f948349a4376d5ec267e1cc556bbfa073d
ramranpara <ramranpara@gmail.com>
2019-06-06 00:12:51 +0530

    some changes

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import Hero from '../components/Hero';
 const Rooms = () => {
-    return <div>hello from room page</div>;
+    return <Hero hero="roomsHero"/>;
 }
 
 export default Rooms
